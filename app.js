a06f0403e443ed62667c41b30e4076b09dd66f93
vibepius <piusvibe0@gmail.com>
2024-09-14 04:00:47 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const images = document.querySelectorAll("img");
+            const images = document.querySelectorAll("img");
 
-    for (const image of images) {
-        fetch("https://dog.ceo/api/breeds/image/random")
-        .then(response => response.json())
-        .then(data => {
-            image.src = data.message
-            image.width = 100;
-            image.height = 100;
-        })
-        
-    }
-});
+            function loadRandomImages() {
+                images.forEach((image) => {
+                    fetch("https://dog.ceo/api/breeds/image/random")
+                    .then(response => response.json())
+                    .then(data => {
+                        image.src = data.message;
+                    });
+                });
+            }
+
+            // Load random images initially
+            loadRandomImages();
+
+            // Set interval to load new random images every 3 seconds
+            setInterval(loadRandomImages, 3000);
+        });
